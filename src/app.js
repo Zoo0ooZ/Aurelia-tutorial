f4199bc89b51759f4c8b1e0affde295c26bc5544
Joseph Bou Saada <josephbousaada@gmail.com>
2017-05-15 21:35:30 -0400

    adding bunch of files

---
+++ src/app.js
@@ -0,0 +1,7 @@
+export class App{
+  constructor(){
+    this.message = "Hello Aurelia";
+    this.image = "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png";
+    this.input = "";
+  }
+}
